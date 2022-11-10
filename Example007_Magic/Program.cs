e642299e00fad2ce0654e43287a0dc3e6283734a
OlgaShestakova <olgashestakova@gmail.com>
2022-11-10 15:38:08 +0300

    Changed Example 007

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 // Console.SetCursorPosition(10, 4);
 // Console.WriteLine("+");
 
-int xa = 1, ya = 1, 
-    xb = 1, yb = 15,
-    xc = 30, yc = 20;
+int xa = 40, ya = 1, 
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -15,3 +15,30 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
+int x = xa, y = ya; 
+
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3); // 0 1 2
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+     if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+     if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+
+}
