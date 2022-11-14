8cc67863c028290277bd621d20d145bf95cdbddd
ShestakovaOlga <olgashestakova@gmail.com>
2022-11-14 14:54:08 +0300

    Changed Example011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -15,11 +15,35 @@ void PrintArray(int[] col)
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(col[position]);
-        position ++;
+        Console.Write(col[position] + " ");
+        position++;
     }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
 }
 
 int[] array = new int[10];
 FillArray(array);
+Console.WriteLine("Сформированный массив:");
 PrintArray(array);
+Console.WriteLine();
+Console.Write("Введите искомый элемент: ");
+int elementNumber = Convert.ToInt32(Console.ReadLine());
+int pos = IndexOf(array, elementNumber);
+if (pos < 0) Console.WriteLine("Элемент не найден");
+else Console.WriteLine($"Данный элемент впервые встречается на позиции {pos}");
